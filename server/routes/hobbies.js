1cd5762bbbcd8e120e6481e744de693e6dc380f6
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-05-28 21:48:13 -0400

    All routers working

--- server/routes/hobbies.js
+++ server/routes/hobbies.js
@@ -12,35 +12,29 @@ module.exports = (db) => {
     }
   });
 
-  // //get hobbies with a specified limit of returned objects
-  // router.get("/limit/:limit", (req, res) => {
-  //   try {
-  //     const hobbies = readHobbiesFile();
-  //     const requestedLimit = req.params.limit;
-  //     const requestedArray = hobbies.slice(0, requestedLimit);
-  //     res.status(200).json(requestedArray);
-  //     if (!requestedArray) {
-  //       return res.status(500).json({ error: "Server error." });
-  //     }
-  //   } catch (e) {
-  //     res.status(404).json({ error: e });
-  //   }
-  // });
+  //get hobbies with a specified limit of returned objects
+  router.get("/limit/:limit", async (req, res) => {
+    try {
+      const requestedLimit = req.params.limit;
+      const hobbiesData = await db("hobbies").select("*").from("hobbies").limit(requestedLimit);
+      res.status(200).json(hobbiesData);
+    } catch (e) {
+      res.status(404).json({ error: e });
+    }
+  });
 
-  // //get one hobby
-  // router.get("/:id", (req, res) => {
-  //   try {
-  //     const hobbies = readHobbiesFile();
-  //     const requestedId = String(req.params.id);
-  //     const requestedHobby = hobbies.find((hobby) => hobby.id == requestedId);
-  //     res.json(requestedHobby);
-  //     if (!requestedHobby) {
-  //       return res.status(500).json({ error: "Server error." });
-  //     }
-  //   } catch (e) {
-  //     res.status(404).json("Hobby was not found.");
-  //   }
-  // });
+  //get one hobby
+  router.get("/id/:id", async (req, res) => {
+    try {
+      
+      const requestedId = Number(req.params.id);
+      console.log(typeof requestedId)
+      const hobbiesData = await db("hobbies").select("*").from("hobbies").where("id", requestedId);
+      res.json(hobbiesData);
+    } catch (e) {
+      res.status(404).json("Hobby was not found.");
+    }
+  });
 
   return router;
 };
