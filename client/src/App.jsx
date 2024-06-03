d5fc3d8014fb87fadd243a9482f39fedb3d398f0
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-06-03 15:14:50 -0400

    Mobile navbar applied

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
 import "./App.scss";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import NavBarMobile from "../../components/NavBarMobile/NavBarMobile";
+import "./HomePage.scss";
+
+export default function HomePage() {
+  return (
+    <>
+      <NavBarMobile />
+    </>
+  );
+}
