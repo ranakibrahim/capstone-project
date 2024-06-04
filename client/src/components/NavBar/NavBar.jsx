35d9681d7e9178f1d0fa22017dda3297eed388e1
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-06-03 21:41:11 -0400

    Navbar done for all devices

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { slide as Menu } from "react-burger-menu";
 import "./NavBar.scss";
 import burgerIcon from "../../assets/icons/burger.svg";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 export default function NavBar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -18,9 +19,11 @@ export default function NavBar() {
     setMenuOpen(!menuOpen);
   };
 
+  const currentloc = useLocation().pathname;
+
   return (
     <>
-      {/*Mobile*/}
+      {/* Mobile */}
       <nav className="navbar navbar--mobile">
         <img
           src={burgerIcon}
@@ -28,34 +31,106 @@ export default function NavBar() {
           className="navbar__burger"
           onClick={toggleMenu}
         />
-        <h1 className="navbar__logo">Hobby Hopper</h1>
-        <Menu isOpen={menuOpen} onStateChange={handleStateChange} right>
-          <a onClick={closeMenu} className="menu-item" href="/">
+        <Link to="/" className="navbar__link navbar__link--mobile">
+          <h1 className="navbar__logo">Hobby Hopper</h1>
+        </Link>
+        <Menu
+          isOpen={menuOpen}
+          onStateChange={handleStateChange}
+          customBurgerIcon={false} // Disable default burger icon
+          customCrossIcon={false} // Disable default cross icon
+          right
+          outerContainerId={"outer-container"} // Ensure correct container id
+          pageWrapId={"page-wrap"} // Ensure correct page wrap id
+        >
+          <Link
+            onClick={closeMenu}
+            className={
+              currentloc === "/"
+                ? "navbar__item navbar__item--active"
+                : "navbar__item"
+            }
+            to="/"
+          >
             Home
-          </a>
-          <a onClick={closeMenu} className="menu-item" href="/about">
+          </Link>
+          <Link
+            onClick={closeMenu}
+            className="navbar__item navbar__item--disabled"
+          >
             About
-          </a>
-          <a onClick={closeMenu} className="menu-item" href="/contact">
+          </Link>
+          <Link
+            onClick={closeMenu}
+            className="navbar__item navbar__item--disabled"
+          >
             Contact
-          </a>
+          </Link>
+          <Link
+            onClick={closeMenu}
+            className={
+              currentloc === "/signup"
+                ? "navbar__item navbar__item--active"
+                : "navbar__item"
+            }
+            to="/signup"
+          >
+            Signup
+          </Link>
+          <Link
+            onClick={closeMenu}
+            className={
+              currentloc === "/login"
+                ? "navbar__item navbar__item--active"
+                : "navbar__item"
+            }
+            to="/login"
+          >
+            Login
+          </Link>
         </Menu>
       </nav>
 
-      {/*Desktop & Tablet*/}
+      {/* Desktop & Tablet */}
       <nav className="navbar navbar--desktoptablet">
-        <h1 className="navbar__logo">Hobby Hopper</h1>
+        <Link to="/" className="navbar__link">
+          <h1 className="navbar__logo">Hobby Hopper</h1>
+        </Link>
         <ul className="navbar__list">
           <Link to="/" className="navbar__link">
-            <li className="navbar__item">Home</li>
+            <li
+              className={
+                currentloc === "/"
+                  ? "navbar__item navbar__item--active"
+                  : "navbar__item"
+              }
+            >
+              Home
+            </li>
           </Link>
           <li className="navbar__item navbar__item--disabled">About</li>
           <li className="navbar__item navbar__item--disabled">Contact</li>
           <Link to="/signup" className="navbar__link">
-            <li className="navbar__item">SignUp</li>
+            <li
+              className={
+                currentloc === "/signup"
+                  ? "navbar__item navbar__item--active"
+                  : "navbar__item"
+              }
+            >
+              Signup
+            </li>
           </Link>
           <Link to="/login" className="navbar__link">
-            <li className="navbar__item">Login</li>
+            <li
+              className={
+                currentloc === "/login"
+                  ? "navbar__item navbar__item--active"
+                  : "navbar__item"
+              }
+            >
+              Login
+            </li>
           </Link>
         </ul>
       </nav>
