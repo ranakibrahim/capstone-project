c999a66ab004059c1942fe05815a9619681b0f89
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-06-04 21:33:50 -0400

    choose hobbies page done

--- src/pages/ChooseHobbyPage/ChooseHobbyPage.jsx
+++ src/pages/ChooseHobbyPage/ChooseHobbyPage.jsx
@@ -1,9 +1,13 @@
 import Profile from "../../components/Profile/Profile";
+import HobbiesList from "../../components/HobbiesList/HobbiesList";
+import "./ChooseHobbyPage.scss";
 
 export default function ChooseHobbyPage() {
   return (
     <main className="hobbies">
       <Profile />
+      <h1 className="hobbies__title">Hello, <span className="hobbies__username">Jane Doe</span></h1>
+      <HobbiesList />
     </main>
   );
 }
