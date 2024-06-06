e1e4cb062597d547157a87dabc78730065263749
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-06-06 16:11:02 -0400

    send hobbies to backend working

--- src/components/HobbiesList/HobbiesList.jsx
+++ src/components/HobbiesList/HobbiesList.jsx
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 
 export default function HobbiesList() {
   const [hobbies, setHobbies] = useState([]);
+  const [chosenHobbyIds, setChosenHobbyIds] = useState([]);
+  const [checkedState, setCheckedState] = useState({});
 
   useEffect(() => {
     getHobbies();
@@ -13,9 +15,57 @@ export default function HobbiesList() {
   const getHobbies = async () => {
     const response = await axios.get(import.meta.env.VITE_API);
     setHobbies(response.data);
+    const initialCheckedState = {};
+
+    response.data.forEach((hobby) => {
+      initialCheckedState[hobby.id] = false;
+    });
+    setCheckedState(initialCheckedState);
   };
+  const handleChecked = (id) => {
+    setCheckedState((prevState) => ({
+      ...prevState,
+      [id]: !prevState[id],
+    }));
+
+    setChosenHobbyIds((prevIds) => {
+      if (prevIds.includes(id)) {
+        return prevIds.filter((hobbyId) => hobbyId !== id);
+      } else {
+        return [...prevIds, id];
+      }
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log(chosenHobbyIds);
+
+    //make axios call
+    try {
+      const response = await axios.post(
+        `${import.meta.env.VITE_USERS}/choose-hobbies`,
+        { hobbyids: chosenHobbyIds },
+        {
+          headers: {
+            Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+          },
+        }
+      );
+      console.log(response.data);
+    } catch (error) {}
+
+    setCheckedState({});
+    setChosenHobbyIds([]);
+  };
+
   return (
     <section className="hobbies-list">
+      <form onSubmit={handleSubmit} className="hobbies-list__form">
+        <button type="submit" className="hobbies-list__submit">
+          Submit
+        </button>
+      </form>
       <h2 className="hobbies-list__title">Choose your hobbies</h2>
       <section className="hobbies-list__all-hobbies">
         {hobbies.map((hobby) => (
@@ -25,6 +75,8 @@ export default function HobbiesList() {
             name={hobby.hobby_name}
             desc={hobby.hobby_description}
             wiki={hobby.hobby_wiki}
+            onChecked={handleChecked}
+            isChecked={checkedState[hobby.id]}
           />
         ))}
       </section>
--- src/components/Hobby/Hobby.jsx
+++ src/components/Hobby/Hobby.jsx
@@ -2,16 +2,25 @@ import React from "react";
 import placeholderImg from "../../assets/images/placeholder.png";
 import "./Hobby.scss";
 
-export default function Hobby({ id, name, desc }) {
+export default function Hobby({ id, name, desc, isChecked, onChecked }) {
+  const handleClick = () => {
+    onChecked(id);
+  };
+
   return (
     <article className="hobby-card">
-      <img
-        src={placeholderImg}
-        alt="hobby image"
-        className="hobby-card__image"
-      />
-      <h5 className="hobby-card__title">{name}</h5>
-      <p className="hobby-card__description">{desc}</p>
+      <section className="hobby-card__content">
+        <img
+          src={placeholderImg}
+          alt="hobby image"
+          className="hobby-card__image"
+        />
+        <h5 className="hobby-card__title">{name}</h5>
+        <p className="hobby-card__description">{desc}</p>
+      </section>
+      <span className="hobby-card__choose" onClick={handleClick}>
+        {isChecked ? <span className="hobby-card__checked"></span> : null}
+      </span>
     </article>
   );
 }
