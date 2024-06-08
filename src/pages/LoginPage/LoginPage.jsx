47c7f67129795278d906f2d19f234178a40e056b
Rana Ibrahim <ranakibrahim02@gmail.com>
2024-06-08 00:49:26 -0400

    added scroll onload to login page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,8 +1,13 @@
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
 import LoginForm from "../../components/LoginForm/LoginForm";
+import { useEffect } from "react";
 
 export default function LoginPage({ setToken }) {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <NavBar />
--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -1,12 +1,12 @@
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
-import SignupForm from "../../components/SignupForm/SignupForm"
+import SignupForm from "../../components/SignupForm/SignupForm";
 export default function LoginPage() {
   return (
     <>
       <NavBar />
-      <SignupForm/>
-      <Footer/>
+      <SignupForm />
+      <Footer />
     </>
   );
 }
